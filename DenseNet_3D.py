029a6467589017cd9d2a3cd89cffac9ee7c8aed9
myubabe <myubabe@proton.me>
2023-03-09 12:59:37 +0000

    update firmware.bin

--- DenseNet_3D.py
+++ DenseNet_3D.py
@@ -12,3 +12,17 @@ def bn_relu_conv(x, filters, kernel_size=(3, 3, 3)):
     return x
 
 def dense_block(x):
+    print('dense block')
+    print(x.get_shape())
+
+    for _ in range(DENSE_NET_BLOCK_LAYERS):
+        y = x
+
+        if DENSE_NET_ENABLE_BOTTLENETCK:
+            y = bn_relu_conv(y, filters=DENSE_NET_GROWTH_RATE, kernel_size=(1, 1, 1))
+
+        y = bn_relu_conv(y, filters=DENSE_NET_GROWTH_RATE, kernel_size=(3, 3, 3))
+        x = Concatenate(axis=4)([x, y])
+        print(x.get_shape())
+
+    return x
