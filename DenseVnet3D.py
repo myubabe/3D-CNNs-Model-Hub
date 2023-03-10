87ed0243d372192b2a1a1732aed671d2c0122bef
myubabe <myubabe@proton.me>
2023-03-10 19:44:31 +0000

    Global refactoring

--- DenseVnet3D.py
+++ DenseVnet3D.py
@@ -5,3 +5,13 @@ from __future__ import division
 import tensorflow as tf
 
 
+
+##########---tf bilinear UpSampling3D
+def up_sampling(input_tensor, scale):
+    net = tf.keras.layers.TimeDistributed(tf.keras.layers.UpSampling2D(size=(scale, scale), interpolation='bilinear'))(input_tensor)
+    net = tf.keras.layers.Permute((2, 1, 3, 4))(net)  # (B, z, H, W, C) -> (B, H, z, w, c)
+    net = tf.keras.layers.TimeDistributed(tf.keras.layers.UpSampling2D(size=(scale, 1), interpolation='bilinear'))(net)
+    net = tf.keras.layers.Permute((2, 1, 3, 4))(net)  # (B, z, H, W, C) -> (B, H, z, w, c)
+    return net
+
+#######-----Bottleneck
