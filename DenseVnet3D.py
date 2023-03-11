eb1f9bf6040b933426294c013854d9c1e7fe9789
myubabe <myubabe@proton.me>
2023-03-11 20:49:08 +0000

    Add installation instruction

--- DenseVnet3D.py
+++ DenseVnet3D.py
@@ -29,3 +29,24 @@ def Bottleneck(x, nb_filter, increase_factor=4., weight_decay=1e-4):
 #####------------>>> Convolutional Block
 def conv_block(input, nb_filter, kernal_size=(3, 3, 3), dilation_rate=1,
                  bottleneck=False, dropout_rate=None, weight_decay=1e-4):
+    ''' Apply BatchNorm, Relu, 3x3X3 Conv3D, optional bottleneck block and dropout
+    Args:
+        input: Input tensor
+        nb_filter: number of filters
+        bottleneck: add bottleneck block
+        dropout_rate: dropout rate
+        weight_decay: weight decay factor
+    Returns: tensor with batch_norm, relu and convolution3D added (optional bottleneck)
+    '''
+
+
+    x = tf.keras.layers.BatchNormalization(epsilon=1.1e-5)(input)
+    x = tf.nn.relu6(x)
+
+    if bottleneck:
+        inter_channel = nb_filter  # Obtained from https://github.com/liuzhuang13/DenseNet/blob/master/densenet.lua
+        x = tf.keras.layers.Conv3D(inter_channel, (1, 1, 1),
+                   kernel_initializer='he_normal',
+                   padding='same',
+                   use_bias=False,
+                   kernel_regularizer=tf.keras.regularizers.l2(weight_decay))(x)
