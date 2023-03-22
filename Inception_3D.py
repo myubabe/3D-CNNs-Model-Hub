ffc0a1f74582b3acd44bd33635aa9d428d26d5ec
myubabe <myubabe@proton.me>
2023-03-22 22:37:57 +0000

    chore(release): [ci skip] publish

--- Inception_3D.py
+++ Inception_3D.py
@@ -1,3 +1,3 @@
 import tensorflow as tf
 from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Input, Conv3D, MaxPooling3D, Den
+from tensorflow.keras.layers import Input, Conv3D, MaxPooling3D, Dense, GlobalMaxPooling
