39ec600892ba6254f01f5673e99aff8accaac478
myubabe <myubabe@proton.me>
2023-03-22 10:18:26 +0000

    Version bump to 0.4.2

--- Inception_3D.py
+++ Inception_3D.py
@@ -1,3 +1,3 @@
 import tensorflow as tf
 from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Input, C
+from tensorflow.keras.layers import Input, Conv3D, MaxPooling3D, Den
