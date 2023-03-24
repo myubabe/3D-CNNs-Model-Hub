4c8515b145eaf56850c65f6c300e31952ca4ca2c
myubabe <myubabe@proton.me>
2023-03-24 13:04:11 +0000

    Fixed #50

--- Inception_3D.py
+++ Inception_3D.py
@@ -1,3 +1,3 @@
 import tensorflow as tf
 from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Input, Conv3D, MaxPooling3D, Dense, GlobalMaxPooling3D, Drop
+from tensorflow.keras.layers import Input, Conv3D, MaxPooling3D, Dense, GlobalMaxPooling3D, Dropout, BatchNorma
