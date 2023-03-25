97f89cb3f0f62eee102b75fc980f51a55d09a752
myubabe <myubabe@proton.me>
2023-03-25 09:45:54 +0000

    fix doc comment

--- Inception_3D.py
+++ Inception_3D.py
@@ -1,3 +1,3 @@
 import tensorflow as tf
 from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Input, Conv3D, MaxPooling3D, Dense, GlobalMaxPooling3D, Dropout, BatchNormalization,Concate
+from tensorflow.keras.layers import Input, Conv3D, MaxPooling3D, Dense, GlobalMaxPooling3D, Dropout, BatchNormalization,Concatenate, Ave
