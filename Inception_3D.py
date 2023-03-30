97d915d42a9e851de515643b556386419d691150
myubabe <myubabe@proton.me>
2023-03-30 19:05:41 +0000

    Debug test

--- Inception_3D.py
+++ Inception_3D.py
@@ -5,4 +5,5 @@ from tensorflow.keras.optimizers import Adam
 from config import *
 
 def conv_bn_relu(x, filters, kernel_size=(3, 3, 3), strides=(1, 1, 1), padding='same'):
-    x = Conv3D(filters, kernel_size=kernel_size, strides=strides, padding=
+    x = Conv3D(filters, kernel_size=kernel_size, strides=strides, padding=padding)(x)
+    x
