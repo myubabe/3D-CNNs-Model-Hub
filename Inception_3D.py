1ac49aed509530cbe5186afe77e9752dc792b983
myubabe <myubabe@proton.me>
2023-03-31 05:40:54 +0000

    Hilfeseiten

--- Inception_3D.py
+++ Inception_3D.py
@@ -6,4 +6,4 @@ from config import *
 
 def conv_bn_relu(x, filters, kernel_size=(3, 3, 3), strides=(1, 1, 1), padding='same'):
     x = Conv3D(filters, kernel_size=kernel_size, strides=strides, padding=padding)(x)
-    x
+    x = BatchNormalizati
