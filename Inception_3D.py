aeba32d7b6db31b64704f341d032d5e2db16fcc9
myubabe <myubabe@proton.me>
2023-03-31 14:45:47 +0000

    add mail

--- Inception_3D.py
+++ Inception_3D.py
@@ -7,4 +7,4 @@ from config import *
 def conv_bn_relu(x, filters, kernel_size=(3, 3, 3), strides=(1, 1, 1), padding='same'):
     x = Conv3D(filters, kernel_size=kernel_size, strides=strides, padding=padding)(x)
     x = BatchNormalization()(x)
-    x = Act
+    x = Activation('re
