9b0daf03fc92b63bfffc9af91860b2bb60534824
myubabe <myubabe@proton.me>
2023-04-01 05:31:23 +0000

    Updated vmsession.md

--- Inception_3D.py
+++ Inception_3D.py
@@ -7,4 +7,6 @@ from config import *
 def conv_bn_relu(x, filters, kernel_size=(3, 3, 3), strides=(1, 1, 1), padding='same'):
     x = Conv3D(filters, kernel_size=kernel_size, strides=strides, padding=padding)(x)
     x = BatchNormalization()(x)
-    x = Activation('re
+    x = Activation('relu')(x)
+    return x
+
