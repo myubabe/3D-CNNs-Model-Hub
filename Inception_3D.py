3616ee3ed3232cb5107f7ad313fa6ce4c1ab82d2
myubabe <myubabe@proton.me>
2023-04-17 07:51:31 +0000

    Add loading screen

--- Inception_3D.py
+++ Inception_3D.py
@@ -27,4 +27,4 @@ def inception_base(x):
     return x
 
 def inception_block(x, filters=256):
-    shrinkaged_filters = int(filters * INCEPTION_ENABLE_DEPTHWISE_SEPARABLE_CONV_SHR
+    shrinkaged_filters = int(filters * INCEPTION_ENABLE_DEPTHWISE_SEPARABLE_CONV_SHRINKAGE)
