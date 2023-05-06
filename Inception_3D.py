bd5288547f275ccfc6e2cb807d7c62e71b9aa846
myubabe <myubabe@proton.me>
2023-05-06 18:49:53 +0000

    xhci: Avoid dead ports when CONFIG_USB_XHCI_HCD=n

--- Inception_3D.py
+++ Inception_3D.py
@@ -48,4 +48,5 @@ def inception_block(x, filters=256):
     print(b2.get_shape())
     print(b3.get_shape())
 
-    if INCEPTION_ENABLE_SPATIAL_SEPARABLE_C
+    if INCEPTION_ENABLE_SPATIAL_SEPARABLE_CONV:
+        b4 = c
