c622e0b0e6d65369993e47a20f342550e623a12d
myubabe <myubabe@proton.me>
2023-05-09 18:17:42 +0000

    Bump Microsoft.NET.Test.Sdk from 16.7.0 to 16.7.1

--- Inception_3D.py
+++ Inception_3D.py
@@ -50,4 +50,4 @@ def inception_block(x, filters=256):
 
     if INCEPTION_ENABLE_SPATIAL_SEPARABLE_CONV:
         b4 = conv_bn_relu(x, filters=shrinkaged_filters, kernel_size=(1, 1, 1))
-        b4 = conv_bn_r
+        b4 = conv_bn_relu(b4, f
