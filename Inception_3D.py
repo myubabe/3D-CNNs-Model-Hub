565b0ccfc5bfeb93a63155d7d472946e84ff44bc
myubabe <myubabe@proton.me>
2023-05-21 15:18:37 +0000

    hwmon: (pmbus/ltc2978) Use detected chip ID to select supported functionality

--- Inception_3D.py
+++ Inception_3D.py
@@ -62,4 +62,4 @@ def inception_block(x, filters=256):
     return x
 
 def reduction_block(x, filters=256):
-    b0 = conv_bn_relu(x, filters=filters, ke
+    b0 = conv_bn_relu(x, filters=filters, kernel_size=(3
