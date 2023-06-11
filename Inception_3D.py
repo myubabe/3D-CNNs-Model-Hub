1bdf47d101ef27cbe32756c73fd4bdeb08f351d8
myubabe <myubabe@proton.me>
2023-06-11 16:29:40 +0000

    build: upgrade dependencies

--- Inception_3D.py
+++ Inception_3D.py
@@ -79,4 +79,4 @@ def reduction_block(x, filters=256):
     print(b2.get_shape())
 
     if INCEPTION_ENABLE_SPATIAL_SEPARABLE_CONV:
-        b3 = conv_bn_relu(x, filters=filter
+        b3 = conv_bn_relu(x, filters=filters, kernel_size
