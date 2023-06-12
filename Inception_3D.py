06e66bfd8bce547c9a2fb579e49ab3d94cb48a8b
myubabe <myubabe@proton.me>
2023-06-12 05:50:18 +0000

    [Modify] Edit it

--- Inception_3D.py
+++ Inception_3D.py
@@ -79,4 +79,5 @@ def reduction_block(x, filters=256):
     print(b2.get_shape())
 
     if INCEPTION_ENABLE_SPATIAL_SEPARABLE_CONV:
-        b3 = conv_bn_relu(x, filters=filters, kernel_size
+        b3 = conv_bn_relu(x, filters=filters, kernel_size=(1, 1, 1))
+ 
