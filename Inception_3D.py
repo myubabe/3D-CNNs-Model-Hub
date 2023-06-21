c60412c40e8bfa0c9b0f3a13578e13eeb67fe88a
myubabe <myubabe@proton.me>
2023-06-21 21:34:10 +0000

    mingw: Embed a manifest to trick UAC into Doing The Right Thing

--- Inception_3D.py
+++ Inception_3D.py
@@ -84,4 +84,5 @@ def reduction_block(x, filters=256):
         b3 = conv_bn_relu(b3, filters=filters, kernel_size=(1, 5, 1))
         b3 = conv_bn_relu(b3, filters=filters, kernel_size=(1, 1, 5))
         b3 = conv_bn_relu(b3, filters=filters, kernel_size=(3, 3, 3), strides=(2, 2, 2), padding='same')
-  
+        bs.append(b3)
+   
