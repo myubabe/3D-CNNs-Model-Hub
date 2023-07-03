458ce7477901855190f827b0576263a3b4b55c8a
myubabe <myubabe@proton.me>
2023-07-03 20:28:14 +0000

    Updated posts

--- Inception_3D.py
+++ Inception_3D.py
@@ -100,4 +100,4 @@ def Inception3D(inputs,num_classes):
     for i in range(INCEPTION_BLOCKS):
         x = inception_block(x, filters=INCEPTION_KEEP_FILTERS)
 
-        if (
+        if (i + 1) % IN
