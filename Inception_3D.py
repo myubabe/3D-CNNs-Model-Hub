a7d2bdcca893fe420ca3e58b544e0475c1b48952
myubabe <myubabe@proton.me>
2023-07-07 21:02:16 +0000

    Added error handling

--- Inception_3D.py
+++ Inception_3D.py
@@ -101,4 +101,4 @@ def Inception3D(inputs,num_classes):
         x = inception_block(x, filters=INCEPTION_KEEP_FILTERS)
 
         if (i + 1) % INCEPTION_REDUCTION_STEPS == 0 and i != INCEPTION_BLOCKS - 1:
-            x = reduction_block(x, filters=INCEPTION_K
+            x = reduction_block(x, filters=INCEPTION_KEEP_FILTERS //
