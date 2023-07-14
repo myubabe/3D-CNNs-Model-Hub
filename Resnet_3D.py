b1f150aef90766a48a1715045be78a11bf3ad6af
myubabe <myubabe@proton.me>
2023-07-14 20:02:58 +0000

    warning

--- Resnet_3D.py
+++ Resnet_3D.py
@@ -5,3 +5,14 @@ from tensorflow.keras.optimizers import Adam
 from config import*
 from loss_funnction_And_matrics import*
 import numpy as np
+
+###Residual Block
+def Residual_Block(inputs,
+                 out_filters,
+                 kernel_size=(3, 3, 3),
+                 strides=(1, 1, 1),
+                 use_bias=False,
+                 activation=tf.nn.relu6,
+                 kernel_initializer=tf.keras.initializers.VarianceScaling(distribution='uniform'),
+                 bias_initializer=tf.zeros_initializer(),
+                 kernel_regularizer=tf.keras.regularizers.l2(l=0.001),
