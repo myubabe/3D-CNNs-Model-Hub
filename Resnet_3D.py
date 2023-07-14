80137b989d143c529c8dd0b360837a10231e6ecf
myubabe <myubabe@proton.me>
2023-07-14 18:54:10 +0000

    windoproj

---
+++ Resnet_3D.py
@@ -0,0 +1,7 @@
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+import tensorflow as tf
+from tensorflow.keras.optimizers import Adam
+from config import*
+from loss_funnction_And_matrics import*
+import numpy as np
