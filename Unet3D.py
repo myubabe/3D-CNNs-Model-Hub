a96b6b46df69159fae3711b49f0dbed78a819844
myubabe <myubabe@proton.me>
2023-07-24 13:49:31 +0000

    fix tox

--- Unet3D.py
+++ Unet3D.py
@@ -1,3 +1,3 @@
 from tensorflow.keras.models import Model
 from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.layers import Conv3D, Input, Ma
+from tensorflow.keras.layers import Conv3D, Input, MaxPooling3D,
