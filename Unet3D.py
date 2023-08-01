782f07ea2e5a37cff2eac60d4518f1c8c0db153c
myubabe <myubabe@proton.me>
2023-08-01 22:21:07 +0000

    upda

--- Unet3D.py
+++ Unet3D.py
@@ -6,4 +6,4 @@ import tensorflow as tf
 def Unet3D(inputs,num_classes):
     x=inputs
     conv1 = Conv3D(8, 3, activation = 'relu', padding = 'same',data_format="channels_last")(x)
-    conv1 = Conv3D(8,
+    conv1 = Conv3D(8, 3, a
