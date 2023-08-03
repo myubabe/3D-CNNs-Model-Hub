8f0424d8f4fe38d61ecd9dcb5728ea573d68f4e6
myubabe <myubabe@proton.me>
2023-08-03 18:10:33 +0000

    Fix linter errors

--- Unet3D.py
+++ Unet3D.py
@@ -7,4 +7,5 @@ def Unet3D(inputs,num_classes):
     x=inputs
     conv1 = Conv3D(8, 3, activation = 'relu', padding = 'same',data_format="channels_last")(x)
     conv1 = Conv3D(8, 3, activation = 'relu', padding = 'same')(conv1)
-    pool1 = MaxPooling3D(pool_size=(2, 2, 2))(
+    pool1 = MaxPooling3D(pool_size=(2, 2, 2))(conv1)
+    
