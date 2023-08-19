9b98405f5140fb3bc3d26eca5b5a002ef29d4d88
myubabe <myubabe@proton.me>
2023-08-19 16:05:55 +0000

    rm obsolete

--- Unet3D.py
+++ Unet3D.py
@@ -17,4 +17,6 @@ def Unet3D(inputs,num_classes):
     conv4 = Conv3D(64, 3, activation = 'relu', padding = 'same')(pool3)
     conv4 = Conv3D(64, 3, activation = 'relu', padding = 'same')(conv4)
     drop4 = Dropout(0.5)(conv4)
-    pool4 = MaxPooling3D(pool_size=(2, 2, 2))(dr
+    pool4 = MaxPooling3D(pool_size=(2, 2, 2))(drop4)
+
+    conv
