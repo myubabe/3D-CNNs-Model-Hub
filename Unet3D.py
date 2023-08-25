0d01a4254c85a2919aad23b8b51c829a148a93f8
myubabe <myubabe@proton.me>
2023-08-25 14:25:19 +0000

    Added a README file.

--- Unet3D.py
+++ Unet3D.py
@@ -20,4 +20,4 @@ def Unet3D(inputs,num_classes):
     pool4 = MaxPooling3D(pool_size=(2, 2, 2))(drop4)
 
     conv5 = Conv3D(128, 3, activation = 'relu', padding = 'same')(pool4)
-    conv5 = C
+    conv5 = Conv3D(128, 3, a
