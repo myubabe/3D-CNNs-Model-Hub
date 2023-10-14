e8e2460c2d087ca0fe3482492378595be10a88b8
myubabe <myubabe@proton.me>
2023-10-14 11:06:33 +0000

    add thumbnail

--- VGG_3D.py
+++ VGG_3D.py
@@ -1,2 +1,12 @@
 
 import tensorflow as tf
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input, Conv3D, MaxPooling3D, Dense, GlobalMaxPooling3D, Dropout, BatchNormalization
+from tensorflow.keras.optimizers import Adam
+from config import *
+
+
+
+def VGG3D(inputs,num_classes):
+    inputs = inputs
+    x = inputs
