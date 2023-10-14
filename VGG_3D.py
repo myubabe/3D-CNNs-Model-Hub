83b50bb13d7d3bd86d0ee45b69db96f32c844ece
myubabe <myubabe@proton.me>
2023-10-14 16:15:30 +0000

    Missing space

--- VGG_3D.py
+++ VGG_3D.py
@@ -10,3 +10,11 @@ from config import *
 def VGG3D(inputs,num_classes):
     inputs = inputs
     x = inputs
+
+
+    x = Conv3D(32, (3, 3, 3), padding='same', activation='relu')(x)
+    x = Conv3D(32, (3, 3, 3), padding='same', activation='relu')(x)
+    x = MaxPooling3D(pool_size=(2, 2, 2))(x)
+
+    if TRAIN_CLASSIFY_USE_BN:
+        x = BatchNormalization()(x)
