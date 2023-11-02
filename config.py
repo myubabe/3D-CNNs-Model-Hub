d0cbdb081d9899b0ade36ec9afe3267f602a9381
myubabe <myubabe@proton.me>
2023-11-02 21:23:50 +0000

    add performance test

--- config.py
+++ config.py
@@ -10,4 +10,5 @@ TRAIN_NUM_RES_UNIT=3
 TRAIN_NUM_FILTERS=(16, 32, 64, 128)
 TRAIN_STRIDES=((1, 1, 1), (2, 2, 2), (2, 2, 2), (2, 2, 2))
 TRAIN_CLASSIFY_ACTICATION=tf.nn.relu6
-TRAIN_KERNAL_INITIALIZER=tf.keras.initializers.VarianceScaling(distribution='uniform'
+TRAIN_KERNAL_INITIALIZER=tf.keras.initializers.VarianceScaling(distribution='uniform')
+#-------DenseNet13
