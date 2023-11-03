84426cbdf7ce46e5b811209861f78cdd2335bfbe
myubabe <myubabe@proton.me>
2023-11-03 09:30:14 +0000

    asdasdas

--- config.py
+++ config.py
@@ -11,4 +11,4 @@ TRAIN_NUM_FILTERS=(16, 32, 64, 128)
 TRAIN_STRIDES=((1, 1, 1), (2, 2, 2), (2, 2, 2), (2, 2, 2))
 TRAIN_CLASSIFY_ACTICATION=tf.nn.relu6
 TRAIN_KERNAL_INITIALIZER=tf.keras.initializers.VarianceScaling(distribution='uniform')
-#-------DenseNet13
+#-------DenseNet13D----#####
